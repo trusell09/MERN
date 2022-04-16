ed843279458e91f6a28f7349e09dba8b99347f75
Trey R <84750467+trusell09@users.noreply.github.com>
2022-04-16 17:56:00 -0500

    Update server.js

--- jokesAPI/server.js
+++ jokesAPI/server.js
@@ -5,16 +5,10 @@ const port = 8000;
 app.use( express.json( ));
 app.use( express.urlencoded({ extended: true}) );
 
-
-
 // connect server to the code in mongoose.config.js file in config folder
 require("./server/config/mongoose.config")
 
-
 //server file sends code to routes file
 require("./server/routes/joke.routes")(app)
-
-
-
 
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
